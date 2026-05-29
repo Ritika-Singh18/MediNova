515a30e1bc6353512eaab352be0f50a5490cf2ea
Ritika Singh <ritikasingh182005@gmail.com>
2026-05-29 23:34:08 +0530

    Fix duplicate api exports

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-import axios from "axios";
-
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
@@ -18,24 +16,6 @@ api.interceptors.request.use(
 
     return config;
   },
-  (error) => Promise.reject(error)
-);
-
-export default api;
-
-/* Request Interceptor */
-
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-  },
-
   (error) => {
     return Promise.reject(error);
   },
